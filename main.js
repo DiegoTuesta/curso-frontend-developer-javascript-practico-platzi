8c8162427399b524ece43f3c83acb971711fa180
Diego Tuesta <diego.productividad@gmail.com>
2023-04-20 12:54:22 -0500

    mobile menu

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const navEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
-navEmail.addEventListener('click', toggleDesktopMenu);
+const mobileMenu = document.querySelector('.mobile-menu');
 
+navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
